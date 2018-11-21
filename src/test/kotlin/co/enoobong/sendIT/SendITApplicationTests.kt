9d8a71dd23f697db591e0012bb2dd12c9d82cfe0
Ibanga Enoobong Ime <ibangaenoobong@yahoo.com>
2018-11-21 19:52:00 +0100

    initialize project

---
+++ src/test/kotlin/co/enoobong/sendIT/SendITApplicationTests.kt
@@ -0,0 +1,16 @@
+package co.enoobong.sendIT
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SendITApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
